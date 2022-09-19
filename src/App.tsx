9b9ac902ef620a9a417e551029e44b08021fe165
Dima Sleznik <dima.sleznik@gmail.com>
2022-09-19 21:32:22 +0400

    add bot init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {Button, Grid} from "@mui/material";
 import EntitySelector from "./components/EntitySelector";
 
 function App() {
+
+  useEffect(() => {
+      const telegram = window.Telegram.WebApp;
+      telegram.ready()
+  },[])
+
   return (
     <Grid>
        <EntitySelector/>
